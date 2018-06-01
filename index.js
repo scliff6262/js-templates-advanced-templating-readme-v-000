d310271fa85c06f31d1d1b480225f5620adb9cf7
Sean Clifford <sclifford6262@gmail.com>
2018-06-01 18:20:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 function loadIssue(){
-  const issue = {
+  /*
+    const issue = {
     state: "closed",
     number: 5,
     created_at: "2016-03-31 16:23:13 UTC",
     body: "Instructions say GET /team and POST /newteam. Rspec wants GET/newteam and POST/team."
   }
-
+*/
   const template = Handlebars.compile(document.getElementById("issue-template").innerHTML)
+  for(let i = 0; i < issues.length; i++){
+    const result = template(issues[i])
 
-  const result = template(issue)
-
-  document.getElementsByTagName("main")[0].innerHTML += result
+    document.getElementsByTagName("main")[0].innerHTML += result
+  }
 }
