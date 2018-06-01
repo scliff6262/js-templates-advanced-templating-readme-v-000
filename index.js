5d958329e21ae4eab04f6e081b9e7c4b68974316
Sean Clifford <sclifford6262@gmail.com>
2018-06-01 18:11:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ function loadIssue(){
     state: "closed",
     number: 5,
     created_at: "2016-03-31 16:23:13 UTC",
-    body: "Instructions say Get /team and POST /newteam. Rspec wants GET/newteam and POST/team."
+    body: "Instructions say GET /team and POST /newteam. Rspec wants GET/newteam and POST/team."
   }
 
   const template = Handlebars.compile(document.getElementById("issue-template").innerHTML)
+
+  const result = template(issue)
+
+  document.getElementsByTagName("main")[0].innerHTML += result
 }
