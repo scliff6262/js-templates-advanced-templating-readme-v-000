5f1528376993e5ca7fcc7cbdf9576253b83f54b9
Sean Clifford <sclifford6262@gmail.com>
2018-06-01 18:34:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,18 +7,9 @@ Handlebars.regiesterHelper('comment_body', function(){
 })
 
 function loadIssues(){
-  /*
-    const issue = {
-    state: "closed",
-    number: 5,
-    created_at: "2016-03-31 16:23:13 UTC",
-    body: "Instructions say GET /team and POST /newteam. Rspec wants GET/newteam and POST/team."
-  }
-*/
   const template = Handlebars.compile(document.getElementById("issue-template").innerHTML)
 
   const result = template(issues)
 
   document.getElementsByTagName("main")[0].innerHTML += result
-
 }
