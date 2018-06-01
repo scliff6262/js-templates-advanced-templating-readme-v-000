122d9fa8326f99b67c34fdf063a41b47cbb57502
Sean Clifford <sclifford6262@gmail.com>
2018-06-01 18:22:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ function loadIssue(){
   }
 */
   const template = Handlebars.compile(document.getElementById("issue-template").innerHTML)
-  for(let i = 0; i < issues.length; i++){
-    const result = template(issues[i])
 
-    document.getElementsByTagName("main")[0].innerHTML += result
-  }
+  const result = template(issues)
+
+  document.getElementsByTagName("main")[0].innerHTML += result
+
 }
